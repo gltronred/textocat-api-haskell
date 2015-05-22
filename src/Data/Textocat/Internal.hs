ad4d619c6fa3fd4e38a3cf6e91f3e6e231ba88f7
Mansur Ziatdinov <gltronred@gmail.com>
2015-05-22 21:59:22 +0300

    Change FromJSON/ToJSON instances generation from TH to Generic

--- src/Data/Textocat/Internal.hs
+++ src/Data/Textocat/Internal.hs
@@ -1,14 +1,16 @@
-﻿{-# LANGUAGE TemplateHaskell #-}
+﻿{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Textocat.Internal where
 
 import           Data.Aeson
-import           Data.Aeson.TH
+import           Data.Aeson.Types
 import           Data.ByteString (ByteString)
 import           Data.Char
 import qualified Data.Text as T
 import           Data.Text (Text)
+import           GHC.Generics
 
 -- | Error message
 type ErrorMsg = String
@@ -23,15 +25,18 @@ data Config = Config
 data Document = Document
     { d_text :: Text -- ^ Text (less than 100 KB)
     , d_tag :: Maybe Text -- ^ Tag (say, user identifier of document)
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 2
-       , constructorTagModifier = map toLower
-       , omitNothingFields = True
-       })
-      ''Document)
+documentOptions = defaultOptions
+    { fieldLabelModifier = drop 2
+    , constructorTagModifier = map toLower
+    , omitNothingFields = True
+    }
+
+instance FromJSON Document where
+  parseJSON = genericParseJSON documentOptions
+instance ToJSON Document where
+  toJSON = genericToJSON documentOptions
 
 -- | Batch identifier (you get it after submitting documents)
 newtype BatchID = BatchID
@@ -41,20 +46,18 @@ newtype BatchID = BatchID
 instance FromJSON BatchID where
   parseJSON (String t) = return $ BatchID t
   parseJSON _ = error "Expected string while reading status"
-
 instance ToJSON BatchID where
   toJSON (BatchID x) = String x
 
 -- | States of batch processing
 data BatchState
-    = FINISHED
-    | IN_PROGRESS
+    = FINISHED    -- ^ Batch is ready for retrieving
+    | IN_PROGRESS -- ^ Batch is being processed
     deriving (Eq,Show,Read)
 
 instance FromJSON BatchState where
   parseJSON (String t) = return $ read $ T.unpack t
   parseJSON _ = error "Expected string while reading status"
-
 instance ToJSON BatchState where
   toJSON x = String $ T.pack $ show x
 
@@ -62,14 +65,17 @@ instance ToJSON BatchState where
 data BatchStatus = BatchStatus
     { bs_batchId :: Text -- ^ Batch ID
     , bs_status :: BatchState -- ^ Batch status
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 3
-       , constructorTagModifier = map toLower
-       })
-      ''BatchStatus)
+bsOptions = defaultOptions
+    { fieldLabelModifier = drop 3
+    , constructorTagModifier = map toLower
+    }
+
+instance FromJSON BatchStatus where
+  parseJSON = genericParseJSON bsOptions
+instance ToJSON BatchStatus where
+  toJSON = genericToJSON bsOptions
 
 -- | States of document processing
 data DocumentState
@@ -81,7 +87,6 @@ data DocumentState
 instance FromJSON DocumentState where
   parseJSON (String t) = return $ read $ T.unpack t
   parseJSON _ = error "Expected string while reading status"
-
 instance ToJSON DocumentState where
   toJSON x = String $ T.pack $ show x
 
@@ -99,7 +104,6 @@ data Category
 instance FromJSON Category where
   parseJSON (String t) = return $ read $ T.unpack t
   parseJSON _ = error "Expected string when reading category"
-
 instance ToJSON Category where
   toJSON x = String $ T.pack $ show x
 
@@ -108,54 +112,66 @@ data Entity = Entity
     , e_beginOffset :: Int
     , e_endOffset :: Int
     , e_category :: Category
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 2
-       , constructorTagModifier = map toLower
-       })
-      ''Entity)
+entityOptions = defaultOptions
+    { fieldLabelModifier = drop 2
+    , constructorTagModifier = map toLower
+    }
+
+instance FromJSON Entity where
+  parseJSON = genericParseJSON entityOptions
+instance ToJSON Entity where
+  toJSON = genericToJSON entityOptions
 
 data AnnotatedDocument = AnnotatedDocument
     { ad_status :: DocumentState
     , ad_tag :: Maybe Text
     , ad_entities :: [Entity]
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 3
-       , constructorTagModifier = map toLower
-       , omitNothingFields = True
-       })
-      ''AnnotatedDocument)
+adOptions = defaultOptions
+    { fieldLabelModifier = drop 3
+    , constructorTagModifier = map toLower
+    , omitNothingFields = True
+    }
+
+instance FromJSON AnnotatedDocument where
+  parseJSON = genericParseJSON adOptions
+instance ToJSON AnnotatedDocument where
+  toJSON = genericToJSON adOptions
 
 data Batch = Batch
     { b_batchIds :: [BatchID]
     , b_documents :: [AnnotatedDocument]
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 2
-       , constructorTagModifier = map toLower
-       })
-      ''Batch)
+batchOptions = defaultOptions
+    { fieldLabelModifier = drop 2
+    , constructorTagModifier = map toLower
+    }
+
+instance FromJSON Batch where
+  parseJSON = genericParseJSON batchOptions
+instance ToJSON Batch where
+  toJSON = genericToJSON batchOptions
 
 data SearchResult = SearchResult
     { sr_searchQuery :: Text
     , sr_documents :: [AnnotatedDocument]
-    } deriving (Eq,Show)
+    } deriving (Eq,Show,Generic)
 
-$(deriveJSON
-      (defaultOptions
-       { fieldLabelModifier = drop 3
-       , constructorTagModifier = map toLower
-       , omitNothingFields = True
-       })
-      ''SearchResult)
+srOptions = defaultOptions
+    { fieldLabelModifier = drop 3
+    , constructorTagModifier = map toLower
+    , omitNothingFields = True
+    }
 
+instance FromJSON SearchResult where
+  parseJSON = genericParseJSON srOptions
+instance ToJSON SearchResult where
+  toJSON = genericToJSON srOptions
+       
 data ServiceStatus
     = ServiceOK
     | ServiceUnavailable
